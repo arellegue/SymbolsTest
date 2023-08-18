a1a55bc5beff71505dc0ee9b712ae7735e1f6b6d
v-arellegue <v-arellegue@microsoft.com>
2023-08-18 10:19:45 -0700

    Added source codes.

---
+++ SymbolsTest/SymbolsTest/SymbolsTestHello.cs
@@ -0,0 +1,14 @@
+﻿namespace SymbolsTest
+{
+    public class SymbolsTestHello
+    {
+        public SymbolsTestHello()
+        {
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine("Hello cruel world!");
+        }
+    }
+}
